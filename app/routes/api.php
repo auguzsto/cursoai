82807fb04f3782342f749313c8e725e94f4d4824
Augusto <auguzsto@gmail.com>
2023-12-23 11:08:27 -0300

    feat: password hash bcrypt

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 error_reporting(0); // SET 0 FOR PROD, SET -1 FOR DEVELOPEMENT
 
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: PATCH, PUT, GET, POST, DELETE');
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Max-Age: 86400');
+
     $router->mount("/api", function() use ($router) {
 
         // Auth
