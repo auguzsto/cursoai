ec206607b085eb28704c022c2cf4022b7599cae8
Augusto <auguzsto@gmail.com>
2024-01-18 16:54:26 -0300

    feat: frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-center items-center h-full">
+        <div class="grid grid-rows-1 gap-2">
+            <div class="flex justify-center items-center">
+                <h1 class="font-bold text-4xl">CursoAí</h1>
+            </div>
+            <div class="flex justify-center items-center">
+                <h1 class="text-xl text-gray-400">Sua plataforma de ensino a distância</h1>
+            </div>
+            <div>
+                <FormLogin />
+            </div>
+        </div>
+    </div>
+</template>
